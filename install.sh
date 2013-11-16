c7431acd52b63ca0fdd467be1c5f7d2937b89103
Michael Parsons <mtparsons87@gmail.com>
2013-11-16 07:25:28 -0500

    Things I usually end up installing on a new box

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt-get update;
+apt-get install python-pip;
+apt-get install git;
+apt-get install emacs24;
+apt-get install libmysqlclient-dev;
+apt-get install screen;
+apt-get install mysql-server;
+apt-get install libssl-dev;
