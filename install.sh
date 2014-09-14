cf4d98db53a3c0ea21255164fb6744e7ffe0d11f
Michael Parsons <mtparsons87@gmail.com>
2014-09-14 10:10:03 -0600

    Adding psql dev libs

--- install.sh
+++ install.sh
@@ -5,12 +5,16 @@ sudo apt-get install python-pip -y;
 sudo apt-get install git -y;
 sudo apt-get install emacs -y;
 sudo apt-get install libmysqlclient-dev -y;
+sudo apt-get install libpq-dev -y;
 sudo apt-get install screen -y;
 sudo apt-get install libssl-dev -y;
 sudo pip install virtualenvwrapper;
 sudo apt-get install nodejs -y;
 sudo apt-get install npm -y;
 sudo apt-get install python-dev -y;
-sudo apt-get install ruby -y;
+sudo apt-get install ruby-dev -y;
 sudo gem install sass;
+sudo gem install compass;
+sudo npm install -g grunt-cli;
+sudo npm install -g bower;
 
