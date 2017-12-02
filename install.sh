3a12c74080eb487ec222a89f7712dd10a64f8d34
Michael Parsons <mtparsons87@gmail.com>
2017-12-02 09:05:23 -0700

    more webdev stuff

--- install.sh
+++ install.sh
@@ -19,3 +19,15 @@ sudo apt-get install curl -y;
 
 #Global Stuff Installed Via PIP
 sudo pip install virtualenvwrapper;
+
+#Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
+#Node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#Angular Cli
+npm install -g @angular/cli
