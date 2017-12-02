c0de1cce511abccd17a4cc10a8dc81804e98a499
Michael Parsons <mtparsons87@gmail.com>
2017-12-02 10:03:16 -0700

    Fixes

--- install.sh
+++ install.sh
@@ -23,11 +23,11 @@ sudo pip install virtualenvwrapper;
 #Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+sudo apt-get update && sudo apt-get install yarn -y
 
 #Node
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 #Angular Cli
-npm install -g @angular/cli
+sudo npm install -g @angular/cli
